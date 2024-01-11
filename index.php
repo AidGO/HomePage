c5d19d7805d69b2f18680c21e095a6dd5fa2d663
AidGO <ayolsen14@gmail.com>
2024-01-11 13:48:09 -0600

    Hw1 Home Page

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
-<!DOCTYPE html>
+<div id="wrapper">
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Home Page!</title>
+<meta charset="utf-8">
+<title>Aiden's Page</title>
+<link rel="stylesheet" href="HomePage.css">
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
+</script>
+<![endif]-->
 </head>
 <body>
-    <h1>
-        GEtting gitbub to work
-    </h1>
+<header> 
+  <h1>Aiden Olsen</h1>
+</header>
+<nav>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="Assignments.php">Assignments</a></li>
+  </ul>
+</nav>
+<main>
+<h2>Why am I taking this class?</h2>
+    <p>
+        I am taking this class to learn more about how to create complex websites that function properly.
+    </p>
+<h2>What do I want to take away from this class?</h2>
+    <p>
+        I want to be able to create proper user interfaces that both show strengh in style and functionality.
+    </p>
+<footer>
+    Github link<br>
+    <a href="https://github.com/AidGO/HomePage">https://github.com/AidGO/HomePage</a>
+</footer>
+</main>
+</div>
 </body>
 </html>
